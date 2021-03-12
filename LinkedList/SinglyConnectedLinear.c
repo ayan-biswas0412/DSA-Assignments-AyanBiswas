c10facf857479616ca80efe3f6d28f6711739d8f
Ayan Biswas <ayanbiswas184@gmail.com>
2021-03-13 01:03:12 +0530

    fix needed for unknown value in deletion function

--- LinkedList/SinglyConnectedLinear.c
+++ LinkedList/SinglyConnectedLinear.c
@@ -15,7 +15,8 @@ struct Node{
 void insert(char location[6],int data,struct Node* *head_ref);
 void insertion_at_beginning(struct Node* *head_ref,int data);
 void insertion_at_end(struct Node* *head_ref,int data);
-void print_linkedlist(struct Node* node);
+void display(struct Node* node);
+void delete(struct Node* *head_ref,int data);
 
 
 int main(){
@@ -23,10 +24,11 @@ int main(){
     struct Node* new_node  = NULL;
     insert("start",5,&new_node);
     insert("end",15,&new_node);
-    print_linkedlist(new_node);
+    insert("end",20,&new_node);
+    delete(&new_node,20);
+    display(new_node);
 
 }
-
 void insert(char location[6],int data,struct Node* *head_ref){
     // The main function for the user to insert a node at the beginning or end based on given choices
     int option = 0;
@@ -86,12 +88,14 @@ void insertion_at_end(struct Node* *head_ref,int data){
             //the list is empty and the new_node should be the first node
             *head_ref = new_node;
         }
+    printf("new node added to the last with value %d\n",data);
 
 
 }
 
-void print_linkedlist(struct Node* node){
+void display(struct Node* node){
     //prints all the elements of a given node
+    printf("\n"); //adding one single line for just visual aspects only
     while (node!= NULL)
     {
         //till not it reaches to last node it prints node data
@@ -99,4 +103,31 @@ void print_linkedlist(struct Node* node){
         node = node->next;
     }
     
+}
+
+void delete(struct Node* *head_ref,int data){
+    //delete a node
+    struct Node* temp = *head_ref;
+    struct Node* prev_node;
+    if(temp->next!=NULL){
+        //checking that the list is empty or not
+        while(temp->data!= data){
+            //traversing the linked list based on the given data
+            prev_node = temp; // tagging the previous node
+            temp = temp->next;
+        }
+        if(temp == NULL){
+            printf("the given element is not present in the list");
+            return; //breaking the operation
+        }
+        //linking the previous node to the next node
+        prev_node->next = temp->next;
+        //finally deletign the node
+        free(temp);
+
+        
+        
+    }else{
+        printf("The list is empty");
+    }
 }
